5d8051839a85257f3711865e2af5833351aa6814
Maxwell Chow <maxchowla@gmail.com>
2021-03-18 03:43:03 -0700

    populate bracket

--- build/index.js
+++ build/index.js
@@ -1,5 +1,65 @@
-var teams = require('bracket-data')({
+var bdata = require('bracket-data')({
     year: '2021',
     sport: 'ncaam'
 });
-console.log(teams);
+var ORDER = bdata.order;
+var REG_SIZE = 16;
+var LAYERS = 4;
+var Team = (function () {
+    function Team(n, s) {
+        this.name = n;
+        this.seed = s;
+    }
+    Team.prototype.toString = function () {
+        return this.name + "(" + this.seed + ")";
+    };
+    return Team;
+}());
+var Matchup = (function () {
+    function Matchup(p) {
+        this.left = null;
+        this.right = null;
+        this.childLeft = null;
+        this.childRight = null;
+        this.parent = p;
+    }
+    Matchup.prototype.toString = function () {
+        return (this.left ? this.left.toString() : "not populated(-)")
+            + " vs " +
+            (this.right ? this.right.toString() : "not populated(-)");
+    };
+    Matchup.prototype.stringAll = function () {
+        return (this.childLeft ? this.childLeft.stringAll() : "") + "\n" +
+            this.toString() +
+            (this.childRight ? this.childRight.stringAll() : "");
+    };
+    return Matchup;
+}());
+for (var _i = 0, _a = bdata.constants.REGION_IDS; _i < _a.length; _i++) {
+    var r = _a[_i];
+    var cregion = bdata.bracket.regions[r].teams;
+    var regfinal = new Matchup(null);
+    var lowestLevel = [regfinal];
+    while (lowestLevel.length < (REG_SIZE / 2)) {
+        var temp = [];
+        for (var _b = 0, lowestLevel_1 = lowestLevel; _b < lowestLevel_1.length; _b++) {
+            var m = lowestLevel_1[_b];
+            var left = new Matchup(m);
+            var right = new Matchup(m);
+            temp.push(left);
+            temp.push(right);
+            m.childLeft = left;
+            m.childRight = right;
+        }
+        lowestLevel = temp;
+    }
+    var count = 0;
+    for (var _c = 0, lowestLevel_2 = lowestLevel; _c < lowestLevel_2.length; _c++) {
+        var m = lowestLevel_2[_c];
+        m.left = new Team(cregion[ORDER[count] - 1], ORDER[count]);
+        m.right = new Team(cregion[ORDER[count + 1] - 1], ORDER[count + 1]);
+        count += 2;
+    }
+    console.log(regfinal.stringAll());
+    break;
+}
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ let bdata = require('bracket-data')({
     sport: 'ncaam'
 });
 
-const ORDER : Array<Number> = bdata.order;
-const REG_SIZE : Number = 16;
-const LAYERS : Number = 4;
+const ORDER : Array<number> = bdata.order;
+const REG_SIZE : number = 16;
+const LAYERS : number = 4;
 
 class Team {
     name: String;
@@ -14,6 +14,9 @@ class Team {
         this.name = n;
         this.seed = s;
     }
+    toString() {
+        return this.name + "(" + this.seed + ")"
+    }
 }
 
 class Matchup {
@@ -21,20 +24,65 @@ class Matchup {
     right: Team;
     childLeft: Matchup | null;
     childRight: Matchup | null;
-    constructor() {
+    parent: Matchup;
+    winner: boolean;
+    constructor(p : Matchup) {
         this.left = null;
         this.right = null;
         this.childLeft = null;
         this.childRight = null;
+        this.parent = p;
+        this.winner = null;
+    }
+    toString() {
+        return (this.left ? this.left.toString() : "not populated(-)") 
+            + " vs " +
+            (this.right ? this.right.toString() : "not populated(-)");
+    }
+
+    stringAll() {
+        //this is flawed
+        //should be a level order traversal but this is inorder tree traversal
+        //which is way easier
+        return (this.childLeft ? this.childLeft.stringAll() : "") + "\n" +
+            this.toString() + 
+            (this.childRight ? this.childRight.stringAll() : "");
     }
 }
 
 for (let r of bdata.constants.REGION_IDS) {
-    let cregion = bdata.bracket.regions[r];
-    let regfinal : Matchup = {
-        left: null,
-        right: null,
-        childLeft: null,
-        childRight: null
-    };
+    let cregion = bdata.bracket.regions[r].teams;
+    let regfinal : Matchup = new Matchup(null);
+    let lowestLevel : Array<Matchup> = [regfinal];
+    while (lowestLevel.length < (REG_SIZE / 2)) {
+        let temp : Array<Matchup> = [];
+        for (let m of lowestLevel) {
+            let left = new Matchup(m);
+            let right = new Matchup(m);
+            temp.push(left);
+            temp.push(right);
+            m.childLeft = left;
+            m.childRight = right;
+        }
+        lowestLevel = temp;
+    }
+    //populate the first matchups
+    let count = 0;
+    for (let m of lowestLevel) {
+        m.left = new Team(cregion[ORDER[count] - 1], ORDER[count]);
+        m.right = new Team(cregion[ORDER[count + 1] - 1], ORDER[count + 1]);
+        count += 2;
+    }
+    // console.log(cregion);
+    // for (let m of lowestLevel) {
+    //     console.log(m.toString());
+    // }
+    console.log(regfinal.stringAll());
+    break;
+}
+
+//true = left win
+function whoWins(seedL, seedR) : boolean {
+    let s = Math.random() * 100 ;
+    return true;
 }
