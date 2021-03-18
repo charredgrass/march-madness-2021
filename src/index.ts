818ae6239daf5eee5de1d856dfbf5c6c27dbd9b1
Maxwell Chow <maxchowla@gmail.com>
2021-03-18 02:56:13 -0700

    basic skeleton for region thingies

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,38 @@ let bdata = require('bracket-data')({
     sport: 'ncaam'
 });
 
+const ORDER : Array<Number> = bdata.order;
+const REG_SIZE : Number = 16;
+const LAYERS : Number = 4;
 
+class Team {
+    name: String;
+    seed: Number;
+    constructor(n : String, s : Number) {
+        this.name = n;
+        this.seed = s;
+    }
+}
+
+class Matchup {
+    left: Team;
+    right: Team;
+    childLeft: Matchup | null;
+    childRight: Matchup | null;
+    constructor() {
+        this.left = null;
+        this.right = null;
+        this.childLeft = null;
+        this.childRight = null;
+    }
+}
+
+for (let r of bdata.constants.REGION_IDS) {
+    let cregion = bdata.bracket.regions[r];
+    let regfinal : Matchup = {
+        left: null,
+        right: null,
+        childLeft: null,
+        childRight: null
+    };
+}
